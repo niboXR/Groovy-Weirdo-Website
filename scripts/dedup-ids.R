c3bf72da88864a05658c3a68a7c186790c1d433c
niboXR <mytrashgeemail@gmail.com>
2026-06-23 20:39:04 -0400

    dedup ids attempt 2

--- scripts/dedup-ids.R
+++ scripts/dedup-ids.R
@@ -1,25 +1,26 @@
-# INCLUDING THIS SCRIPT CAUSED MY LOGO TO DISSAPEAR #
-
-
-library(xml2)
 
 html_files <- list.files("docs", pattern = "\\.html$", recursive = TRUE, full.names = TRUE)
 
 for (filepath in html_files) {
-  doc <- read_html(filepath)
+  html <- readLines(filepath, encoding = "UTF-8", warn = FALSE)
+  text <- paste(html, collapse = "\n")
   
   seen <- c()
-  nodes <- xml_find_all(doc, "//*[@id]")
+  matches <- gregexpr('id="([^"]+)"', text, perl = TRUE)
+  ids <- regmatches(text, matches)[[1]]
   
-  for (node in nodes) {
-    id <- xml_attr(node, "id")
-    if (id %in% seen) {
-      xml_attr(node, "id") <- NULL
+  for (id_attr in ids) {
+    id_val <- sub('id="([^"]+)"', "\\1", id_attr)
+    if (id_val %in% seen) {
+      text <- sub(paste0(' id="', id_val, '"'), "", text, fixed = TRUE)
     } else {
-      seen <- c(seen, id)
+      seen <- c(seen, id_val)
+      text <- sub(paste0('id="', id_val, '"'), paste0('id="KEEP_', id_val, '"'), text, fixed = TRUE)
     }
   }
   
-  write_html(doc, filepath)
+  text <- gsub('id="KEEP_', 'id="', text, fixed = TRUE)
+  
+  writeLines(text, filepath, useBytes = FALSE)
   cat("Deduplicated:", filepath, "\n")
 }
